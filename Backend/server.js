13204e9fd422a76bab368e41f8b09bb706f9840f
Pavanika_15 <23eg105v04@anurag.edu.in>
2026-05-19 21:15:14 +0530

    Fix CORS for deployed frontend

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ config(); //process.env
 //Create express application
 const app = exp();
 //use cors middleware
-app.use(cors({ origin: ["http://localhost:5173",], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", process.env.FRONTEND_URL], credentials: true }));
 
 //add body parser middleware
 app.use(exp.json());
